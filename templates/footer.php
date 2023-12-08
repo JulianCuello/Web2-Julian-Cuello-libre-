d57e84a63e608f796f225f66b16b0fe9f03a6612
julian Cuello <julianc_69@hotmail.com>
2023-12-08 20:15:03 -0300

    solucionando errores querys

---
+++ templates/footer.php
@@ -0,0 +1,13 @@
+<!-- main footer -->
+</div>
+<footer class="bg-dark text-white text-center fixed-bottom py-2">
+    <div class="container">
+        <p>2023 TUDAI UNICEN - Integrantes: Cuello Julian</p>
+
+    </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ templates/header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- head -->
+<head>
+    <meta charset="UTF-8">
+    <base href="<?php echo BASE_URL; ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>TUDAI -Trabajo WEB</title>
+</head>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="list">Listado <span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="category">Categorias</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="form">Agregar items</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="listAdmin">Gestion items</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="formCategory">Agregar categorias</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="categoryAdmin">Gestion categorias</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="category" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          SeleccionarCategoria
+        </a>
+      
+      </li>
+    </ul>
+  </div>
+</nav>
+<body>
+    <div class="fondo d-flex justify-cotent-center align-items-center">
+
+    </div>
+</body>
